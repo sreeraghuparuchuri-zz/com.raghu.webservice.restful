d34da97d4731196f2eaebf616b967b696f990697
Sree Raghu Paruchuri <raghu4466@hotmail.com>
2016-10-03 09:23:52 +0530

    Insert and Fetch data from database
    finalized the putRecords method which will insert docs into database..
    
    getRecords from Database — W.I.P

--- src/com/raghu/webservice/restful/dbservices/DbServices.java
+++ src/com/raghu/webservice/restful/dbservices/DbServices.java
@@ -1,7 +1,9 @@
 package com.raghu.webservice.restful.dbservices;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import org.bson.Document;
 
@@ -25,6 +27,12 @@ public class DbServices {
 	
 	public String getRecords() {
 		
+		Document doc = new Document();
+		StringBuilder sbReturn = new StringBuilder();
+		String strFname = "";
+		String strLname = "";
+		int iAge ;
+		
 		connectToDB();
 		
 		MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("myCollection");
@@ -32,20 +40,67 @@ public class DbServices {
 		List<Document> foundDocument = mongoCollection.find().into(new ArrayList<Document>());
 		System.out.println("foundDocument>>>"+foundDocument);
 		
-		Document doc = foundDocument.get(0);
-		System.out.println("doc>>>"+doc);
+
 		
-		String strID = doc.get("_id").toString();
-		System.out.println("strID>>>"+strID);	
 		
-		String x = doc.get("x").toString();
-		System.out.println("x>>>"+x);
+		Iterator<Document> itr = foundDocument.iterator();
+		
+		while (itr.hasNext()) {
+			
+			doc = itr.next();
+			System.out.println("doc >>>>"+doc);
+			sbReturn.append(doc.toJson());
+			sbReturn.append(",");
+			System.out.println(" Document to JSON >>> "+ doc.toJson());
+			System.out.println(" Document >>> "+ doc.get("FName"));
+			System.out.println(" Document >>> "+ doc.get("LName"));
+			System.out.println(" Document >>> "+ doc.get("Age"));
+			
+		}
+		
+		System.out.println("sbReturn >>>"+sbReturn);
+//		Document doc = foundDocument.get(6);
+//		System.out.println("doc>>>"+doc);
+//		
+//		String strID = doc.get("_id").toString();
+//		System.out.println("strID>>>"+strID);	
+//		
+//		String FNAME = doc.get("FName").toString();
+//		System.out.println("FNAME>>>"+FNAME);
+//		
+//		String LNAME = doc.get("LName").toString();
+//		System.out.println("LNAME>>>"+LNAME);
+//		
+//		String AGE = doc.get("Age").toString();
+//		System.out.println("AGE>>>"+AGE);
 		
 		mongoClient.close();
-		return "Ret Value";
+		return doc.toString();
 	}
 	
-	public String putRecords() {
+	public String putRecords(String strRecord) {
+		
+		System.out.println("strRecord >>> "+strRecord);
+		String[] strValues = strRecord.split("\\|");
+		
+		//System.out.println("Length of strValues >>>"+strValues.length);
+		
+		
+		connectToDB();
+		
+		MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("myCollection");
+		
+		Document insertDoc = new Document();
+		for(int i=0; i<= strValues.length -1; i++)
+		{	
+			
+			insertDoc.put("FName", strValues[0]);
+			insertDoc.put("LName", strValues[1]);
+			insertDoc.put("Age", strValues[2]);
+			
+		}
+		
+		mongoCollection.insertOne(insertDoc);
 		
 		return "Ret Value";
 	}
--- src/com/raghu/webservice/restful/test/TestClass.java
+++ src/com/raghu/webservice/restful/test/TestClass.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -18,16 +19,40 @@ public class TestClass {
 	public String hello() {
 		DbServices dbServ = new DbServices();
 		String str = dbServ.getRecords();
-		System.out.println(str);
-		return "Hello World!";
+		System.out.println("getData From TestClass"+str);
+		return str;
 	}
 	
-	@POST
+	@PUT
 	@Produces("text/plain")
 	@Path("/addnew")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public String addNew(String input) {
+		
 		System.out.println("Input>>>"+input);
+		String[] str1 = null;
+		StringBuffer sb = new StringBuffer();
+		String strValue = "";
+		
+		input = input.replace('"', ' ').replace('{', ' ').replace('}', ' ').replaceAll("\\s",""); 
+		System.out.println("Input after replace >>>"+input);
+		
+		String[] splitInput = input.split(",");
+		//int len = splitInput.length;
+		
+		for( int i = 0; i <= splitInput.length - 1; i++)
+		{
+			str1 = splitInput[i].split(":");
+			//System.out.println("str1 >>>"+str1[1]);
+			sb.append(str1[1]);
+			sb.append("|");
+			
+		}
+		
+		System.out.println("Values From Test Class ..."+sb.toString());
+		DbServices dbServ = new DbServices();
+		dbServ.putRecords(sb.toString());
+		
 		return "Request Received";
 		
 	}
